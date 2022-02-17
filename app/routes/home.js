218c99a01095e200f8e1a44cd247a5721497801b
Sean Chen <qianlonwork@outlook.com>
2022-02-17 22:13:51 +0800

    fix: 🐛 修复 cross-env 以及 router 文件未引出问题

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,8 +1,16 @@
-const Router =require('koa-router')
+/*
+ * @Author: qianlong github:https://github.com/LINGyue-dot
+ * @Date: 2022-02-17 22:11:40
+ * @LastEditors: qianlong github:https://github.com/LINGyue-dot
+ * @LastEditTime: 2022-02-17 22:13:14
+ * @Description:
+ */
+const Router = require("koa-router");
 
-const router =new Router({prefix:'/'}) // prefix
+const router = new Router({ prefix: "/" }); // prefix
 
-const home =require('../controllers/home')
+const home = require("../controllers/home");
 
-router.get("/",home.getHomeList)
+router.get("/", home.getHomeList);
 
+module.exports = router;
